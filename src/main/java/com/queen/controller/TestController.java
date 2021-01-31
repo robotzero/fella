0a8f642432ef7aa15844ffcc45b77f9fb16b7fe2
Agnieszka K. Kocinski <0robotzero@gmail.com>
2021-01-31 15:32:37 +0000

    Auth

--- src/main/java/com/queen/controller/TestController.java
+++ src/main/java/com/queen/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.queen.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,17 +10,19 @@ import reactor.core.publisher.Mono;
 
 @RestController
 public class TestController {
-  @GetMapping("/test")
+  @GetMapping("/messages")
   @ResponseStatus(HttpStatus.OK)
-  public Mono<Product> test() {
-    return getProduct();
+  public Mono<String[]> test(Authentication authentication) {
+	  JwtAuthenticationToken jwtAuthentication = (JwtAuthenticationToken) authentication;
+    return getProduct(jwtAuthentication.getToken().getTokenValue());
   }
 
-  private Mono<Product> getProduct() {
+  private Mono<String[]> getProduct(String token) {
     return Mono.fromSupplier(() -> {
-      final var product = new Product();
-      product.setId(14);
-      return product;
+//      final var product = new Product();
+//      product.setId(14);
+//      return product;
+      return new String[] {"Message 1", "Message 2", "Message 4", token};
     });
   }
 }
--- src/main/java/com/queen/infrastructure/FellaConfiguration.java
+++ src/main/java/com/queen/infrastructure/FellaConfiguration.java
@@ -1,17 +1,20 @@
 package com.queen.infrastructure;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
-@EnableWebFluxSecurity
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
+@EnableWebFluxSecurity
 public class FellaConfiguration {
   @Bean
   public SecurityWebFilterChain securityWebFilterChain(
       ServerHttpSecurity http) {
-    return http.authorizeExchange().pathMatchers("/actuator/**").permitAll().anyExchange().permitAll().and().build();
+//    return http.authorizeExchange().pathMatchers("/actuator/**").permitAll().anyExchange().permitAll().and().build();
+	  return http.authorizeExchange().pathMatchers("/messages/**").hasAuthority("SCOPE_message.read").anyExchange().authenticated().and().oauth2ResourceServer().jwt(withDefaults()).and().build();
   }
 }
