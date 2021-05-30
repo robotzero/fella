876a73aa5a0474e0b10221b88aa0a31ce47f77be
Agnieszka K. Kocinski <0robotzero@gmail.com>
2021-05-30 15:49:03 +0100

    started hatoas

--- src/main/java/com/queen/adapters/web/controller/MonitorTypeController.java
+++ src/main/java/com/queen/adapters/web/controller/MonitorTypeController.java
@@ -9,10 +9,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @RestController
 public class MonitorTypeController {
@@ -25,17 +26,16 @@ public class MonitorTypeController {
 	}
 
 	@GetMapping("/monitor-types")
-	//@TODO errors, 404 etc
-	ResponseEntity<Flux<MonitorTypeDTO>> loadMonitorTypes(
+	//@TODO errors
+	Mono<ResponseEntity<List<MonitorTypeDTO>>> loadMonitorTypes(
 			@CurrentSecurityContext(expression = "authentication.userId") String userId,
 			@RequestParam(name = "page", defaultValue = PageSupportDTO.FIRST_PAGE_NUM) int page,
 			@RequestParam(name = "size", defaultValue = PageSupportDTO.DEFAULT_PAGE_SIZE) int size
 	) {
-		return ResponseEntity.status(HttpStatus.OK).body(allMonitorTypesQuery.load(userId, PageRequest.of(page, size)).map(monitorType -> monitorTypeToDTO.toDTO(monitorType)));
-	}
-
-	@PostMapping("/monitor-types")
-	void createNewMonitorType() {
-
+		return allMonitorTypesQuery.load(userId, PageRequest.of(page, size)).map(monitorType -> {
+			return monitorTypeToDTO.toDTO(monitorType);
+		}).collectList().map(monitoryTypeDTOs -> {
+			return ResponseEntity.status(HttpStatus.OK).body(monitoryTypeDTOs);
+		}).defaultIfEmpty(ResponseEntity.notFound().build());
 	}
 }
