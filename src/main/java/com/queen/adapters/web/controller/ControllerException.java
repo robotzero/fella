8f025aaa1ba8efaba6a9eab5f685bf12a0386a48
Agnieszka K. Kocinski <0robotzero@gmail.com>
2022-05-04 20:20:01 +0100

    Upgraded to M2

--- src/main/java/com/queen/adapters/web/controller/ControllerException.java
+++ src/main/java/com/queen/adapters/web/controller/ControllerException.java
@@ -7,11 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import reactor.core.publisher.Mono;
 
 @ControllerAdvice
-public class ControllerException extends ResponseEntityExceptionHandler {
+public class ControllerException {
 
 	@ExceptionHandler({MonitorTypeException.class})
 	public Mono<ResponseEntity<ExceptionDTO>> handleMonitorTypeException(final Exception exception) {
--- src/main/java/com/queen/infrastructure/persitence/MonitorTypeRepository.java
+++ src/main/java/com/queen/infrastructure/persitence/MonitorTypeRepository.java
@@ -2,12 +2,12 @@ package com.queen.infrastructure.persitence;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.r2dbc.repository.Query;
-import org.springframework.data.repository.reactive.ReactiveSortingRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 
 import java.util.List;
 
-public interface MonitorTypeRepository extends ReactiveSortingRepository<MonitorType, String> {
+public interface MonitorTypeRepository extends ReactiveCrudRepository<MonitorType, String> {
 	@Query("SELECT mt.* FROM monitor_type mt INNER JOIN fields f ON f.monitorTypeId = mt.id INNER JOIN field_type ft ON ft.id = f.fieldTypeId WHERE mt.userId = :userId GROUP BY mt.id ORDER BY mt.created_at LIMIT :#{#pageable.getPageSize()} OFFSET :#{#pageable.getOffset()}")
 	Flux<MonitorType> findByUserId(String userId, Pageable pageable);
 
