c20dedaf54a999f820115ea36bb3ad045fe8ac3e
Agnieszka K. Kocinski <0robotzero@gmail.com>
2025-08-26 11:40:42 +0100

    java 25

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:24
+FROM openjdk:25
 ARG JAVA_ARGS=
 ENV JAVA_ARGS=${JAVA_ARGS}
 #ARG JAR_FILE=target/*.jar
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 group = "com.queen"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_24
+java.sourceCompatibility = JavaVersion.VERSION_25
 
 repositories {
     mavenCentral()
