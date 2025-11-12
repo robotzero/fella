5584487d491f70117eecd1423285aa08f422bffa
Agnieszka K. Kocinski <0robotzero@gmail.com>
2025-11-12 20:53:58 +0000

    edit cont

--- src/main/java/com/queen/domain/DailyTracking.java
+++ src/main/java/com/queen/domain/DailyTracking.java
@@ -1,7 +1,8 @@
 package com.queen.domain;
 
 import java.time.LocalDate;
+import java.util.Optional;
 import java.util.UUID;
 
-public record DailyTracking(UUID userId, LocalDate trackingDate, Integer painLevel, Integer flowLevel) {
+public record DailyTracking(Optional<UUID> trackingId, UUID userId, LocalDate trackingDate, Integer painLevel, Integer flowLevel) {
 }
